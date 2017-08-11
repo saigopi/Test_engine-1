787a8a5e40938ff0cdf16fff152a1b5d6d2c6297
vaishnavij <vaishnavi.j3@gmail.com>
2017-08-11 15:21:35 +0530

    [poornima\Vaishnavi] Adding sonarqube for test reporting

--- src/test/java/functional/CalculatorTest.java
+++ src/test/java/functional/CalculatorTest.java
@@ -35,5 +35,76 @@ public class CalculatorTest {
         int divide = test.divide(10, 10);
         Assert.assertEquals(1, divide);
     }
-}
 
+    public void addTest1(){
+        int addValue = test.addition(10, 12);
+        Assert.assertEquals(2, addValue);
+    }
+
+    @Test
+    public void subTest1(){
+        int subValue = test.subtraction(10, 12);
+        Assert.assertEquals(-28, subValue);
+    }
+
+    @Test
+    public void multiplyTest1(){
+        int multiply = test.multiply(10, 12);
+        Assert.assertEquals(120, multiply);
+    }
+
+    @Test
+    public void divisionTest1(){
+        int divide = test.divide(10, 10);
+        Assert.assertEquals(1, divide);
+    }
+
+    @Test
+    public void addTest2(){
+        int addValue = test.addition(10, 12);
+        Assert.assertEquals(2, addValue);
+    }
+
+    @Test
+    public void subTest2(){
+        int subValue = test.subtraction(10, 12);
+        Assert.assertEquals(-23, subValue);
+    }
+
+    @Test
+    public void multiplyTest2(){
+        int multiply = test.multiply(10, 12);
+        Assert.assertEquals(120, multiply);
+    }
+
+    @Test
+    public void divisionTest2(){
+        int divide = test.divide(10, 10);
+        Assert.assertEquals(10, divide);
+    }
+
+    @Test
+    public void addTest4(){
+        int addValue = test.addition(10, 12);
+        Assert.assertEquals(22, addValue);
+    }
+
+    @Test
+    public void subTest4(){
+        int subValue = test.subtraction(10, 12);
+        Assert.assertEquals(-2, subValue);
+    }
+
+    @Test
+    public void multiplyTest4(){
+        int multiply = test.multiply(10, 12);
+        Assert.assertEquals(10, multiply);
+    }
+
+    @Test
+    public void divisionTest4(){
+        int divide = test.divide(10, 10);
+        Assert.assertEquals(10, divide);
+    }
+
+}
